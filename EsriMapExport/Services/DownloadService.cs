7d931988a955d171b3d4f2ed429777ec94f52cfd
Bole <jelenabole@gmail.com>
2018-10-17 11:50:43 +0200

    changed services namespace

--- EsriMapExport/Services/DownloadService.cs
+++ EsriMapExport/Services/DownloadService.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace EsriMapExport.Controllers
+namespace EsriMapExport.Services
 {
     class DownloadService
     {
--- EsriMapExport/Services/MapService.cs
+++ EsriMapExport/Services/MapService.cs
@@ -7,7 +7,7 @@ using System.Globalization;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace EsriMapExport.Controllers
+namespace EsriMapExport.Services
 {
 	class MapService
 	{
